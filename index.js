227eb761be682a67eaf07915321cbe85e31085da
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-02 14:17:39 +0100

    Add favicon

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { defaultSuccess, defaultError } = require('./controllers/default.controller.js');
 const { getAllPokemon } = require('./controllers/pokemon.controller.js');
+const favicon = require('serve-favicon');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.use(favicon(`${__dirname}/images/favicon.png`));
 
 app.get('/api', defaultSuccess);
 
