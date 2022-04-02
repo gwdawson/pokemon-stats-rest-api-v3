f84edea7dbfe15fac73f8dc51b95b5f01ae653a0
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-02 10:31:57 +0100

    Add tests for default routes

---
+++ tests/default.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const app = require('../index.js');
+
+describe('Testing for the GET /api endpoint', () => {
+  test('should return a json object with the keys {status, message, endpoints}', async () => {
+    const { body } = await request(app).get('/api');
+    expect(body).toHaveProperty('status');
+    expect(body).toHaveProperty('message');
+    expect(body).toHaveProperty('endpoints');
+  });
+  test('response.status should equal 200', async () => {
+    const { body } = await request(app).get('/api');
+    expect(body.status).toBe(200);
+  });
+  test('response.message should equal "Welcome to the pokemon-api-v3"', async () => {
+    const { body } = await request(app).get('/api');
+    expect(body.message).toBe('Welcome to the pokemon-api-v3');
+  });
+  test('response.endpoints should equal []', async () => {
+    const { body } = await request(app).get('/api');
+    expect(body.endpoints).toEqual([]);
+  });
+});
+
+describe('Testing for the ALL /* endpoint', () => {
+  test('should return a json objext with the keys {status, message, default}', async () => {
+    const { body } = await request(app).get('/invalid');
+    expect(body).toHaveProperty('status');
+    expect(body).toHaveProperty('message');
+    expect(body).toHaveProperty('default');
+  });
+  test('response.status should equal 440', async () => {
+    const { body } = await request(app).get('/invalid');
+    expect(body.status).toBe(404);
+  });
+  test('response.message should equal "Not Found"', async () => {
+    const { body } = await request(app).get('/invalid');
+    expect(body.message).toBe('Not Found');
+  });
+  test('response.endpoints should equal "/api"', async () => {
+    const { body } = await request(app).get('/invalid');
+    expect(body.default).toBe('/api');
+  });
+});
