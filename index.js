90940def86be6f2cf71eb61c688cbccb9cd2f767
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-02 14:54:34 +0100

    Add request logging

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const { defaultSuccess, defaultError } = require('./controllers/default.controll
 const { getAllPokemon } = require('./controllers/pokemon.controller.js');
 const favicon = require('serve-favicon');
 const express = require('express');
+const fs = require('fs/promises');
 const app = express();
 
 app.use(express.json());
 
 app.use(favicon(`${__dirname}/images/favicon.png`));
+
+app.use((req, res, next) => {
+  const { method, originalUrl } = req;
+  const data = `A (${method}) request was made to (${originalUrl}) at ${new Date()}\n`;
+  fs.appendFile(`./logs/request.log`, data).then(() => next());
+});
 
 app.get('/api', defaultSuccess);
 
